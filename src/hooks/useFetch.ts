bf36288882ed494039efc91b1d60c9269a1c3ae0
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-12 17:28:23 +0900

    feat: useFetch 커스텀 훅 생성

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,69 @@
+import { useEffect, useRef, useReducer } from 'react';
+
+type stateType = {
+  status: string;
+  error?: string | null;
+  data?: actionType[];
+};
+
+type actionType = {
+  type: string;
+  payload?: [];
+};
+
+export const useFetch = (url: string) => {
+  const cache = useRef<any>({});
+
+  const initialState = {
+    status: 'idle',
+    error: null,
+    data: [],
+  };
+
+  const [state, dispatch] = useReducer((state: any, action: actionType) => {
+    switch (action.type) {
+      case 'FETCHING':
+        return { ...initialState, status: 'fetching' };
+      case 'FETCHED':
+        return { ...initialState, status: 'fetched', data: action.payload };
+      case 'FETCH_ERROR':
+        return { ...initialState, status: 'error', error: action.payload };
+      default:
+        return state;
+    }
+  }, initialState);
+
+  useEffect(() => {
+    let cancelRequest = false;
+
+    if (!url || !url.trim()) {
+      dispatch({ type: 'FETCHED', payload: [] });
+      return function cleanup() {
+        cancelRequest = true;
+      };
+    }
+
+    const fetchData = async () => {
+      dispatch({ type: 'FETCHING' });
+      if (cache.current[url]) {
+        console.log('from cache');
+        const data = cache.current[url];
+        dispatch({ type: 'FETCHED', payload: data.data });
+      } else {
+        console.log('from api');
+        const response = await fetch(url);
+        const data = await response.json();
+        cache.current[url] = data;
+        if (cancelRequest) return;
+        dispatch({ type: 'FETCHED', payload: data.data });
+      }
+    };
+
+    fetchData();
+    return function cleanup() {
+      cancelRequest = true;
+    };
+  }, [url]);
+
+  return state;
+};
